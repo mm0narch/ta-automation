c72f87ae4ade7ca64578d2c4e2a171206eecca4c
mm0narch <mauritiusmanurungnightfury@gmail.com>
2025-04-07 14:53:23 +0700

    add: new account page

--- src/app/(pages)/newaccount/page.tsx
+++ src/app/(pages)/newaccount/page.tsx
@@ -14,16 +14,12 @@ export default function LoginPage() {
     router.push('/dashboard');
   };
 
-  const newAccount = () => {
-    router.push('/newaccount');
-  }
-
   return (
     <div 
       className="min-h-screen flex items-center justify-center bg-cover bg-center"
       style={{backgroundImage: "url('/background.jpg')"}}>
       <div className="bg-[#f9f9f9] p-8 rounded-sm shadow-xl w-88">
-        <h1 className="mt-5 text-2xl font-bold mb-7 text-center text-neutral-950">Login</h1>
+        <h1 className="mt-5 text-2xl font-bold mb-7 text-center text-neutral-950">New User</h1>
         <div className="mb-2">
           <label className="text-sm font-medium text-black text-neutral-900">username</label>
           <input
@@ -59,7 +55,7 @@ export default function LoginPage() {
         </div>
         <button
           onClick={handleLogin}
-          className="mt-11 mb-9 w-full p-2 text-white rounded-xl bg-cover bg-center relative overflow-hidden"
+          className="mt-12 mb-10 w-full p-2 text-white rounded-xl bg-cover bg-center relative overflow-hidden"
           style={{
             backgroundImage: "url('/background.jpg')",
             backgroundSize: "cover",
@@ -67,16 +63,8 @@ export default function LoginPage() {
           }}
         >
           <div className="absolute inset-0 bg-black/30 hover:bg-black/20 transition-all duration-300"></div>
-          <span className='relative z-10'>LOGIN</span>
+          <span className='relative z-10'>PROCEED</span>
         </button>
-        <div className='flex flex-col items-end'>
-          <button
-            onClick={newAccount}
-            className='mb-2 text-sm hover:underline transition font-semibold text-[#ee0035]'
-          >
-            Create New User
-          </button>
-        </div>
       </div>
     </div>
   );
