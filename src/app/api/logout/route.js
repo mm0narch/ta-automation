c86463a1a76ccfcb7db889e271a3d2afab899767
mm0narch <mauritiusmanurungnightfury@gmail.com>
2025-04-22 12:06:20 +0700

    add: session logout

---
+++ src/app/api/logout/route.js
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+import { supabase } from "../../../lib/supabase";
+
+export async function POST(req) {
+    const sessionCookie = req.cookies.get('session')
+    
+    //db
+    try {
+    if (sessionCookie?.value) {
+        await supabase()
+            .from('sessions')
+            .delete()
+            .eq('token', sessionCookie.value)            
+        }
+    } catch(error) {
+        console.error('Failed to remove session from DB', error)
+    }
+            
+    const response = NextResponse.json({ success:true })
+    response.cookies.delete('session')
+    
+    return response
+}
