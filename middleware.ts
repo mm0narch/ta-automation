a731c18b8f138fb6305a1967b3252c9bf271086d
mm0narch <mauritiusmanurungnightfury@gmail.com>
2025-04-29 23:33:11 +0700

    add: middleware role

--- middleware.ts
+++ middleware.ts
@@ -30,21 +30,42 @@ export async function middleware(request: NextRequest) {
   const supabase = createClient(supabaseUrl, supabaseKey);
   
   //checks cookie expiration  
-  const { data, error } = await supabase
+  const { data: session, error: sessionError } = await supabase
     .from('sessions')
     .select('*')
     .eq('token', sessionCookie.value)
     .gt('expires_at', new Date().toISOString())
     .single();
   
-  //in invalid returns to login page
-  if (error || !data) {
+  //if invalid returns to login page
+  if (sessionError || !session) {
     const response = NextResponse.redirect(new URL('/dashboard', request.url));
     response.cookies.delete('session');
     return response;
   }
+
+  //checks user credentials
+  const { data: user, error: userError } = await supabase
+    .from('users')
+    .select('role')
+    .eq('id', session.user_id)
+    .single()
+
+  //user role check
+  if (userError || !user) {
+    return NextResponse.redirect(new URL('/dashboard', request.url));
+  }
+  if (path.startsWith('/admin') && user.role !=='admin') {
+    return NextResponse.redirect(new URL('/unauthorized', request.url))
+  }
+  if (path.startsWith('/doctor') && user.role !=='doctor') {
+    return NextResponse.redirect(new URL('/unauthorized', request.url))
+  }
+  if (path.startsWith('/pharma') && user.role !=='pharma') {
+    return NextResponse.redirect(new URL('/unauthorized', request.url))
+  }
   
-  //everything checks
+  //everything checks 
   return NextResponse.next();
 }
 
